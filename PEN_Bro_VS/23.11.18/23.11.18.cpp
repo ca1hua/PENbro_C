0862eed73cd44c6d401a9976c3fce4bf8118e42a
chenxi <1478475274@qq.com>
2023-11-19 01:48:53 +0800

    //11.19

--- PEN_Bro_VS/23.11.18/23.11.18.cpp
+++ PEN_Bro_VS/23.11.18/23.11.18.cpp
@@ -1,7 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+//库函数
+
+//int main()
+//{
+//	printf("别太逆天hello world");
+//	//print 打印 f formation
+//	printf("%d",'w');
+//
+//	printf("%f", 3.5f);
+//	printf("%lf", 3.5);
+//	printf("%c\n", 'A'+32);
+//
+//}
+//关键字，
+// hex 16
+// oct 8
+//字符0是48        0-31是不可打印的字符
+//  
+//int main()
+//{
+//
+//	char arr1[] = "abc";
+//	char arr2[] = {'a','b','c'};
+//	//隐藏\0   十分重要
+//	//调试F10
+//	return 0;
+//	//《函数战争》
+//}
+
+
+//int main()
+//{
+//	printf("abc\nc");
+//}
 
 int main()
 {
-	printf("别太逆天hello world");
+	printf("%c", '\'');
+	return 0;
 }
+//\b  可回退一个 /r 回到头
