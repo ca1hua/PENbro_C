8509cad42bc7e01a2d573a3508eea935dbb0a9db
chenxi <1478475274@qq.com>
2023-11-21 00:43:28 +0800

    //11.21

--- PEN_Bro_VS/2023.11.20/除余.cpp
+++ PEN_Bro_VS/2023.11.20/除余.cpp
@@ -1,9 +1,70 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS 
+//#include <stdio.h>
+//
+//int main() {
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//    printf("%d %d", a / b, a % b);
+//    return 0;
+//}
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d %d", a / b, a % b);
-    return 0;
+#include <stdio.h>
+//int num = 10;
+//int main()
+//{
+//    int num = 1;//先是这个
+//    printf("num = %d\n", num);
+//    return 0;
+//}
+
+//int main()
+//{
+//	printf("%c",'\130');
+//	printf("%c", '\x30');
+//	//          \ddd   \130就是八进制表示ASCll码
+//}
+#include <string.h>
+//int main()
+//{
+//	int len = strlen("abc");
+//	printf("%d\n", len);
+//	//转义字符就是会
+//	//        strlen 求的是\0之前的字符说
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for (i = 0; i < 10; i++) //for循环的循环体的⼤括号中的就是复合语句
+//	{
+//		arr[i] = 10 - i;
+//		printf("%d\n", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//C语言是一种结构话=化的程序设计语言
+
+//int main()
+//{
+//	/*
+//	是大陆空军，我jdalFLAG{多行}
+//	dfa
+//	fdsjh
+//	da
+//	*/          //不支持嵌套的
+//	return 0;
+//}
+
+// 注释会被替换为空格
+
+
+
+int main()
+{
+	
+	return 0;
 }
